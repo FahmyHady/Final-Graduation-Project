78f6d0a7b259884d264486e63f5022e0ce7582b3
Iarm5 <amir_lion@live.com>
2019-06-22 18:57:12 +0200

    New UI Added

--- Final Project Prototype/Assets/Amir/Scripts/UI/Canvas/Player/StaminaUpdater.cs
+++ Final Project Prototype/Assets/Amir/Scripts/UI/Canvas/Player/StaminaUpdater.cs
@@ -6,20 +6,19 @@ public class StaminaUpdater : MonoBehaviour
     #region Fields
     [SerializeField] private Image fillImg;
     [SerializeField] private PlayerStateInfo info;
-    [SerializeField] private Slider slider;
     #endregion Fields
 
     #region Methods
     public void UpdateStamina()
     {
         fillImg.color = info.Player.Outline;
-        slider.value = info.CurrentStamina / info.MaxStamina;
+        fillImg.fillAmount = info.CurrentStamina / info.MaxStamina;
     }
 
     private void Start()
     {
         fillImg.color = info.Player.Outline;
-        slider.value = info.CurrentStamina / info.MaxStamina;
+        fillImg.fillAmount = info.CurrentStamina / info.MaxStamina;
     }
     #endregion Methods
 }
